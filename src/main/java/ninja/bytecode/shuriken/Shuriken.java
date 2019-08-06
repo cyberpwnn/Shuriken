2138ae57acdca97e9ff137bb8b0f135847010491
cyberpwn <danielmillst@gmail.com>
2019-08-06 13:38:15 -0400

    Removals

--- src/main/java/ninja/bytecode/shuriken/Shuriken.java
+++ src/main/java/ninja/bytecode/shuriken/Shuriken.java
@@ -1,29 +1,8 @@
 package ninja.bytecode.shuriken;
 
 import ninja.bytecode.shuriken.bench.Profiler;
-import ninja.bytecode.shuriken.execution.ChronoLatch;
-import ninja.bytecode.shuriken.execution.QueueExecutor;
 
 public class Shuriken
 {
 	public static final Profiler profiler = new Profiler();
-	
-	public static void main(String[] a)
-	{
-		ChronoLatch c = new ChronoLatch(250);
-		QueueExecutor q = new QueueExecutor();
-		q.start();
-		int e = 0;
-		
-		while(true)
-		{
-			q.queue().queue(() -> {});
-			e++;
-			
-			if(c.flip())
-			{
-				System.out.println("MS: " + q.getRunTime() + " E: " + e);
-			}
-		}
-	}
 }
--- src/main/java/ninja/bytecode/shuriken/math/RNG.java
+++ src/main/java/ninja/bytecode/shuriken/math/RNG.java
@@ -9,7 +9,7 @@ public class RNG extends Random
 	private static final char[] CHARGEN = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-=!@#$%^&*()_+`~[];',./<>?:\\\"{}|\\\\".toCharArray();
 	private static final long serialVersionUID = 5222938581174415179L;
 	public static final RNG r = new RNG();
-	
+
 	public RNG() {
 		super();
 	}
@@ -17,7 +17,7 @@ public class RNG extends Random
 	public RNG(long seed) {
 		super(seed);
 	}
-	
+
 	/**
 	 * Creates a seed (long) from the hash of the seed string
 	 * @param seed the seed (string)
@@ -26,24 +26,24 @@ public class RNG extends Random
 	{
 		this(UUID.nameUUIDFromBytes(seed.getBytes(StandardCharsets.UTF_8)).getLeastSignificantBits() + UUID.nameUUIDFromBytes(seed.getBytes(StandardCharsets.UTF_8)).getMostSignificantBits() + (seed.length() * 32564));
 	}
-	
+
 	public String s(int length)
 	{
 		StringBuilder sb = new StringBuilder();
-		
+
 		for(int i = 0; i < length; i++)
 		{
 			sb.append(c());
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	public char c()
 	{
 		return CHARGEN[i(CHARGEN.length - 1)];
 	}
-	
+
 	/**
 	 * Pick a random enum
 	 * @param t the enum class
@@ -54,7 +54,7 @@ public class RNG extends Random
 		T[] c = t.getEnumConstants();
 		return c[i(c.length)];
 	}
-	
+
 	public boolean b()
 	{
 		return nextBoolean();
@@ -64,72 +64,72 @@ public class RNG extends Random
 	{
 		return d() > percent;
 	}
-	
-	public short si(int lowerBound, int upperBound) 
+
+	public short si(int lowerBound, int upperBound)
 	{
 		return (short) (lowerBound + (nextFloat() * ((upperBound - lowerBound) + 1)));
 	}
-	
+
 	public short si(int upperBound)
 	{
 		return si(0, upperBound);
 	}
-	
+
 	public short si()
 	{
 		return si(1);
 	}
-	
-	public float f(float lowerBound, float upperBound) 
+
+	public float f(float lowerBound, float upperBound)
 	{
-		return lowerBound + (nextFloat() * ((upperBound - lowerBound) + 1));
+		return lowerBound + (nextFloat() * ((upperBound - lowerBound)));
 	}
-	
+
 	public float f(float upperBound)
 	{
 		return f(0, upperBound);
 	}
-	
+
 	public float f()
 	{
 		return f(1);
 	}
-	
-	public double d(double lowerBound, double upperBound) 
+
+	public double d(double lowerBound, double upperBound)
 	{
-		return lowerBound + (nextDouble() * ((upperBound - lowerBound) + 1));
+		return lowerBound + (nextDouble() * ((upperBound - lowerBound)));
 	}
-	
+
 	public double d(double upperBound)
 	{
 		return d(0, upperBound);
 	}
-	
+
 	public double d()
 	{
 		return d(1);
 	}
-	
+
 	public int i(int lowerBound, int upperBound)
 	{
-		return (int) d(lowerBound, upperBound);
+		return (int) Math.round(d(lowerBound, upperBound));
 	}
-	
+
 	public int i(int upperBound)
 	{
 		return i(0, upperBound);
 	}
-	
+
 	public long l(long lowerBound, long upperBound)
 	{
-		return (long) l(lowerBound, upperBound);
+		return Math.round(d(lowerBound, upperBound));
 	}
-	
+
 	public long l(long upperBound)
 	{
 		return l(0, upperBound);
 	}
-	
+
 	public int imax()
 	{
 		return i(Integer.MIN_VALUE, Integer.MAX_VALUE);
@@ -139,18 +139,18 @@ public class RNG extends Random
 	{
 		return l(Long.MIN_VALUE, Long.MAX_VALUE);
 	}
-	
+
 	public float fmax()
 	{
 		return f(Float.MIN_VALUE, Float.MAX_VALUE);
 	}
-	
+
 	public double dmax()
 	{
 		return d(Double.MIN_VALUE, Double.MAX_VALUE);
 	}
-	
-	public short smax()
+
+	public short simax()
 	{
 		return si(Short.MIN_VALUE, Short.MAX_VALUE);
 	}
