cb81e500be4a0e924f20c6cf008b1ae22c604762
Daniel Mills <danielmillst@gmail.com>
2019-08-27 13:03:29 -0700

    Queries

---
+++ src/main/java/ninja/bytecode/shuriken/sql/Query.java
@@ -0,0 +1,71 @@
+package ninja.bytecode.shuriken.sql;
+
+import ninja.bytecode.shuriken.collections.GList;
+
+public class Query
+{
+	private StringBuilder query;
+
+	public Query()
+	{
+		query = new StringBuilder();
+	}
+	
+	public Query selectAll()
+	{
+		query.append(" ");
+		query.append("SELECT *");
+		return this;
+	}
+
+	public Query select(String... select)
+	{
+		//@builder
+		query.append(" ");
+		query.append("SELECT " + new GList<String>()
+				.add(select)
+				.convert((a) -> "`" + a + "`").toString(", "));
+		//@done
+		return this;
+	}
+
+	public Query from(String in)
+	{
+		query.append(" ");
+		query.append("FROM `" + in + "`");
+		return this;
+	}
+
+	public Query whereEquals(String v, String eq)
+	{
+		query.append(" ");
+		query.append("WHERE `" + v + "` = '" + eq + "'");
+		return this;
+	}
+
+	public Query whereAll()
+	{
+		query.append(" ");
+		query.append("WHERE 1");
+		return this;
+	}
+
+	public Query where(String where)
+	{
+		query.append(" ");
+		query.append("WHERE " + where);
+		return this;
+	}
+
+	public Query limit(int offset, int pageSize)
+	{
+		query.append(" ");
+		query.append("LIMIT " + offset + "," + pageSize);
+		return this;
+	}
+
+	public String toString()
+	{
+		return query.toString().trim() + ";";
+	}
+}
