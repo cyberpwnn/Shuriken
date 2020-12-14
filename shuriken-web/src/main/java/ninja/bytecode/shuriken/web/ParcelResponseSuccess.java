239ab2171ae137e2a3c8b3b7848821c806e74550
Daniel Mills <danielmillst@gmail.com>
2020-12-13 19:25:29 -0500

    FML

---
+++ shuriken-web/src/main/java/ninja/bytecode/shuriken/web/ParcelResponseSuccess.java
@@ -0,0 +1,15 @@
+package ninja.bytecode.shuriken.web;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface ParcelResponseSuccess
+{
+	public Class<? extends Parcelable> type();
+	public String reason();
+}
